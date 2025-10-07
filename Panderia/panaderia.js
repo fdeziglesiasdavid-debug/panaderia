800aaffe1f9827213f745304e16916d9ba0cb9fb
fdeziglesiasdavid-debug <fdeziglesiasdavid@gmail.com>
2025-10-07 13:17:24 +0200

    Delete Panderia directory

--- Panderia/panaderia.js
+++
@@ -1,7 +0,0 @@
-// Actualiza automáticamente el año en el footer
-document.addEventListener('DOMContentLoaded', () => {
-  const year = document.getElementById('year');
-  if (year) {
-    year.textContent = new Date().getFullYear();
-  }
-});
